e56e72309755f1074bf708602636997ee88eb90a
ksisjdjwoaomddjkw <150491467+ksisjdjwoaomddjkw@users.noreply.github.com>
2026-08-17 17:53:50 -0700

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,46 @@
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+import {
+    getDatabase
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyB1P2P-3dR6DDKyEbglxQ4ElGd5au006n4",
+
+    authDomain: "test-1619e.firebaseapp.com",
+
+    databaseURL:
+        "https://test-1619e-default-rtdb.firebaseio.com/",
+
+    projectId:
+        "test-1619e",
+
+    storageBucket:
+        "test-1619e.firebasestorage.app",
+
+    messagingSenderId:
+        "374500672512",
+
+    appId:
+        "1:374500672512:web:6c5529a84b56ce017204e5"
+};
+
+
+const app =
+    initializeApp(firebaseConfig);
+
+
+export const auth =
+    getAuth(app);
+
+
+export const database =
+    getDatabase(app);
